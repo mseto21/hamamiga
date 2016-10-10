6d0754ccbc76c0857c62b141e0f9a36fb8e660bc
mkosarek <mkosare1@gmail.com>
2016-10-10 18:14:27 -0400

    Fixed indentation

--- src/Constants.h
+++ src/Constants.h
@@ -4,10 +4,10 @@
 
 namespace Constants {
 	const uint16 	ScreenWidth_ 	= 	640;
-	const uint16 	ScreenHeight_ = 	480;
+	const uint16 	ScreenHeight_ 	= 	480;
 	const float   	StepSize_ 		= 	8.f;
-	const uint32  	NumKeys_			=		323;
-	const uint32	MaxEntities_  =   128;
+	const int  		NumKeys_		=	323;
+	const uint32	MaxEntities_  	=   128;
 	const float 	TargetFps_ 		= 	60.f;
 	const float 	OptimalTime_ 	= 	1000 / TargetFps_;
 
@@ -20,15 +20,15 @@ namespace Constants {
 	// Play State
 	const uint16 	PlayerWSize_ 	=	33; //player image size
 	const uint16 	PlayerHSize_ 	=	60; //player image size
-	const float    Gravity_                =      0.5; //gravity
-        const float        Friction_           =      1; //friction
-	const uint16    MaxVY_                =      16; //max y velocity
-	const uint16    MaxVX_             =        6; //max x velocity
-	const uint16    Jump_                =       16; //jump velocity
-        const float        Accel_                 =       6; //acceleration
-	const uint16  DemonHSize_   = 60; 
-  const uint16  DemonWSize_   = 61;// demon image size
-	const uint16 Damage_             =        20; //damage
+	const float    	Gravity_        =   0.5; //gravity
+    const float     Friction_    	=   1; //friction
+	const uint16    MaxVY_          =   16; //max y velocity
+	const uint16    MaxVX_          =   6; //max x velocity
+	const uint16    Jump_           =   16; //jump velocity
+    const float     Accel_       	=   6; //acceleration
+	const uint16  	DemonHSize_   	= 	60; 
+  	const uint16 	DemonWSize_   	= 	61;// demon image size
+	const uint16 	Damage_         =   20; //damage
 
 	// High Score State
 	const int     MaxHighScores_	=	5;
@@ -37,7 +37,7 @@ namespace Constants {
 	const String128	TitleBackground_	= 	"title_background";
 	const String128	TitleFader_			=	"title_fader";
 	const String128	WinBackground_		= 	"win_background";
-	const String128     LoseBackground_      =      "lose_background";
+	const String128 LoseBackground_ 	=   "lose_background";
 	const String128 GameBackground_ 	= 	"game_background";
 	const String128 MenuOverlay_    	= 	"menu_overlay";
 }
