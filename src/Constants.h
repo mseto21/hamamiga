226da3931967191f0eff82eb32c1848f9deaacf2
mkosarek <mkosare1@gmail.com>
2016-10-16 17:04:15 -0400

    Fixed systems

--- src/Constants.h
+++ src/Constants.h
@@ -14,6 +14,7 @@ namespace Constants {
 
 	const uint16	MaxMapWidth_	=	256; // In number of tiles
 	const uint16	MaxMapHeight_	=	256;
+	const uint16	TileSize_		=	42;
 
 	// Intro State
 	const float		IntroTime_		= 	250.f;
