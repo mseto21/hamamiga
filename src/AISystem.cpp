8ec066cbec95c60aa024967f0185a6a0f6ac12e8
hcohen23 <harry.m.cohen@gmail.com>
2016-10-16 20:55:26 -0400

    fixed ai need to slow down enemies

--- src/AISystem.cpp
+++ src/AISystem.cpp
@@ -9,7 +9,7 @@
 #include <SDL.h>
 #include <iostream>
 
-bool collides(const Rectangle* r1, const Rectangle* r2);
+bool close(const Rectangle* r1, const Rectangle* r2);
 
 void AISystem_Update(AIComponent* aiComponent, MovementComponent* movementComponent, RectangleComponent* rectangleComponent, float timeStep) {
   Rectangle pRect = rectangleComponent->entityRectangles[rectangleComponent->entityArray[Constants::PlayerIndex_]];
@@ -26,10 +26,10 @@ void AISystem_Update(AIComponent* aiComponent, MovementComponent* movementCompon
       continue;
     }
     MovementValues* moveValues = &movementComponent->movementValues[eid];
-    Rectangle* eRect = &rectangleComponent->entityRectangles[0];
+    Rectangle* eRect = &rectangleComponent->entityRectangles[eid];
     moveValues->xAccel = 0;
-    if (collides(&pRect, eRect)) {
-  	  if (pRect.x < eRect->x) {
+    if (close(&pRect, eRect)) {
+      if ((pRect.x+Constants::Range_ + pRect.w/Constants::Range_)/2 < (eRect->x + eRect->w)/2) {
   	    moveValues->xAccel = -Constants::Accel_;
   	  } else {
   	    moveValues->xAccel = Constants::Accel_;
@@ -37,7 +37,7 @@ void AISystem_Update(AIComponent* aiComponent, MovementComponent* movementCompon
   	} else {
   	  MarchValues* marchValues = &aiComponent->marchValues[eid];
   	  moveValues->xAccel = Constants::Accel_*marchValues->facing;
-  	  marchValues->distance += moveValues->xAccel*timeStep;
+  	  marchValues->distance += moveValues->xAccel*timeStep*marchValues->facing;
   	  if (marchValues->distance >= marchValues->range) {
   	    marchValues->distance = 0;
   	    marchValues->facing *= -1;
@@ -47,8 +47,9 @@ void AISystem_Update(AIComponent* aiComponent, MovementComponent* movementCompon
   }
 }
 
-bool collides(const Rectangle* r1, const Rectangle* r2) {
-    if (r1->x <= r2->x + r2->w && r1->x + r1->w >= r2->x && r1->y <= r2->y + r2->h && r1->y + r1->h >= r2->y) {
+bool close(const Rectangle* r1, const Rectangle* r2) {
+  if (abs((r1->x+r1->w)/2 - (r2->x+r2->w)/2) < Constants::Range_) {
+    std::cout << "in here" << std::endl;
     return true;
   }
   return false;
