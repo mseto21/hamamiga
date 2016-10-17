2b7547c4e237a66b5605f3354ced62e54ac888e4
mkosarek <mkosare1@gmail.com>
2016-10-17 19:22:34 -0400

    Format issue

--- src/CameraSystem.cpp
+++ src/CameraSystem.cpp
@@ -25,20 +25,19 @@ void CameraSystem_Update(CameraSystem* cameraSystem) {
 		camera->y = ( rectangle.y + Constants::PlayerHSize_ / 2 ) - Constants::ScreenWidth_ / 2;
 
 		//Keep the camera in bounds
-		if( camera->x < 0 )
-		{ 
+		if (camera->x < 0) { 
 			camera->x = 0;
 		}
-		if( camera->y < 0 )
-		{
+
+		if (camera->y < 0) {
 			camera->y = 0;
 		}
-		if( camera->x > Constants::LevelWidth_ - camera->w )
-		{
+
+		if (camera->x > Constants::LevelWidth_ - camera->w) {
 			camera->x = Constants::LevelWidth_  - camera->w;
 		}
-		if( camera->y > Constants::LevelHeight_  - camera->h )
-		{
+		
+		if (camera->y > Constants::LevelHeight_  - camera->h) {
 			camera->y = Constants::LevelHeight_  - camera->h;
 		}
 	}
