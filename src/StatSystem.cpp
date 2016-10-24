d81b764e07e10de95dff9c2285dd94df12820ec3
hcohen23 <harry.m.cohen@gmail.com>
2016-10-23 21:20:55 -0400

    got rid of warnings

--- src/StatSystem.cpp
+++ src/StatSystem.cpp
@@ -8,6 +8,7 @@ void StatSystem_Initialize(StatSystem* statSystem, ComponentBag* cBag) {
 
 
 void StatSystem_Update(StatSystem* statSystem) {
+        (void) statSystem;
 	/*int* health = &game->playState.cBag.healthComponent->health[Constants::PlayerIndex_];
 	int* y = &game->playState.cBag.rectangleComponent->entityRectangles[Constants::PlayerIndex_].y;
 	if (*health <= 0 || *y >= Constants::LevelHeight_) {
