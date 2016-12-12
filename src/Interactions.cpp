7dec077726e74b87467d5d439a563421c2571c2c
mkosarek <mkosare1@gmail.com>
2016-12-12 10:31:30 -0500

    You can now remove hats

--- src/Interactions.cpp
+++ src/Interactions.cpp
@@ -196,6 +196,10 @@ void Interaction_RemoveHatInteraction(uint32 eid, ComponentBag* cBag) {
 			cBag->movementComponent->movementValues[eid].accelY /= -1;
 			cBag->movementComponent->movementValues[eid].drunk = false;
 			break;
+		case HatTypes_Chef:
+		case HatTypes_Cowboy:
+			Component_ForceRemove(cBag->bulletComponent, eid);
+			break;
 		default:
 			return;
 	}
